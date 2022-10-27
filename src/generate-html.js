e88d736f41cf5e01e32cec17d272a401edb333b5
Darby Biddy <darbybiddy3@gmail.com>
2022-10-26 19:38:59 -0500

    made some final changes to the styles

--- src/generate-html.js
+++ src/generate-html.js
@@ -60,14 +60,17 @@ function generateTeam(team) {
 
 module.exports = (team) => {
   return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="../src/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+      <title>Document</title>
+  </head>
     <body>
         ${generateTeam(team)}
     </body>
