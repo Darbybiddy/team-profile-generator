3e0794124e783779b021a50db38490cb96b32f28
Darby Biddy <darbybiddy3@gmail.com>
2022-11-21 17:30:52 -0600

    styling changes

--- src/generate-html.js
+++ src/generate-html.js
@@ -1,6 +1,7 @@
 function generateTeam(team) {
   const createManager = (manager) => {
     return `
+ 
         <div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">${manager.getName()}</h5>
@@ -9,24 +10,28 @@ function generateTeam(team) {
           <p><a href="mailto:${manager.getEmail()}" class="card-link">${manager.getEmail()}</a></p>
           <p class="card-text">${manager.getOfficeNumber()}</p>
         </div>
-      </div>`;
+      </div>
+     `;
   };
 
   const createEngineer = (engineer) => {
     return `
+   
         <div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">${engineer.getName()}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${engineer.getRole()}</h6>
           <p class="card-text">${engineer.getId()}</p>
           <p><a href="mailto:${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a></p>
-          <p class="card-text">${engineer.getGithub()}</p>
+          <p class="card-text">${engineer.getGitHub()}</p>
         </div>
-      </div>`;
+      </div>
+      `;
   };
 
   const createIntern = (intern) => {
     return `
+  
         <div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">${intern.getName()}</h5>
@@ -57,8 +62,8 @@ function generateTeam(team) {
   );
   return html.join("");
 }
-
-module.exports = (team) => {
+// need to figure out what html goes here.
+TODO: module.exports = (team) => {
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -69,10 +74,23 @@ module.exports = (team) => {
       <link rel="stylesheet" href="../src/style.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+      <link rel="stylesheet" href="./style.css">
       <title>Document</title>
   </head>
     <body>
+    <header>
+    <h1>Team Profile Generator</h1>
+  </header>
+  <main>
+  <div class="row">
+  <div class="d-flex flex-wrap justify-content-center">
         ${generateTeam(team)}
+        </div>
+        </div>
+         </main>
+         <footer class="container text-center py-2">
+         <h5 class="text-white bg-dark ">&copy;2022 Darby Biddy </h5>
+     </footer>
     </body>
     </html>
     `;
